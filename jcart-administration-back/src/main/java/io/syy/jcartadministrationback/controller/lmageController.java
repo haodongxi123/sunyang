41729d3b9b6cf6294ac67c4176d70aa2650cf990
DESKTOP-86LR7NS\Mac <1497377019@qq.com>
2020-02-24 18:44:50 +0800

    add image controller

---
+++ jcart-administration-back/src/main/java/io/syy/jcartadministrationback/controller/lmageController.java
@@ -0,0 +1,18 @@
+package io.syy.jcartadministrationback.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/image")
+public class lmageController {
+
+    @PostMapping("/upload")
+    public  String update(@RequestParam MultipartFile image){
+            
+        return "";
+    }
+}
