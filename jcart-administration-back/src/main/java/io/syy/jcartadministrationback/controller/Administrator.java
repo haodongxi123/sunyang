2309d35d4d587f71a91fcdb602edfbf9d896142c
DESKTOP-86LR7NS\Mac <1497377019@qq.com>
2020-02-24 19:14:08 +0800

    draft some api

---
+++ jcart-administration-back/src/main/java/io/syy/jcartadministrationback/controller/Administrator.java
@@ -0,0 +1,27 @@
+package io.syy.jcartadministrationback.controller;
+
+import io.syy.jcartadministrationback.dto.in.AdministratorLogInDTO;
+import io.syy.jcartadministrationback.dto.in.AdministratorUpdateProfileInDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/administrator")
+public class Administrator {
+
+        @GetMapping("/login")
+        public  String login(AdministratorLogInDTO administratorLogInDTO){
+            return null;
+        }
+
+        @GetMapping("/getProfile")
+        public  AdministratorLogInDTO getProfile(Integer adminstratorId){
+            return null;
+        }
+
+        @PostMapping("/updateProdfile")
+         public void updateProdfile(@RequestBody AdministratorUpdateProfileInDTO administratorUpdateProfileInDTO){
+
+        }
+
+        
+}
--- jcart-administration-back/src/main/java/io/syy/jcartadministrationback/controller/lmageController.java
+++ jcart-administration-back/src/main/java/io/syy/jcartadministrationback/controller/lmageController.java
@@ -12,7 +12,7 @@ public class lmageController {
 
     @PostMapping("/upload")
     public  String update(@RequestParam MultipartFile image){
-            
+
         return "";
     }
 }
