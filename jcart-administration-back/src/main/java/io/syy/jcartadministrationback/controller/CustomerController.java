1545b8eaf619c683c91a6507c0ca2eadad07ca07
DESKTOP-86LR7NS\Mac <1497377019@qq.com>
2020-02-25 17:02:08 +0800

    add

---
+++ jcart-administration-back/src/main/java/io/syy/jcartadministrationback/controller/CustomerController.java
@@ -0,0 +1,28 @@
+package io.syy.jcartadministrationback.controller;
+
+import io.syy.jcartadministrationback.dto.in.CustomerSearchInDTO;
+import io.syy.jcartadministrationback.dto.out.CustomerListOutDTO;
+import io.syy.jcartadministrationback.dto.out.CustomerShowOutDTO;
+import io.syy.jcartadministrationback.dto.out.PageOutDTO;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerController {
+
+        @GetMapping("/search")
+        public PageOutDTO<CustomerListOutDTO> search(CustomerSearchInDTO customerSearchInDTO, @RequestParam Integer pageNum){
+            return null;
+        }
+
+        @GetMapping("/getById")
+        public CustomerShowOutDTO getById(@RequestParam Integer customerId){
+            return null;
+        }
+
+        @PostMapping("/disable")
+        public  void disable(@RequestParam Integer customerId){
+            
+        }
+
+}
