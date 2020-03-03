b6a77c927db549b1fad365cf0a313bbc2aee12f0
DESKTOP-86LR7NS\Mac <1497377019@qq.com>
2020-03-03 16:46:59 +0800

    添加成功

--- jcart-store-front/viewmodels/product-show.js
+++ jcart-store-front/viewmodels/product-show.js
@@ -36,17 +36,28 @@ var app = new Vue({
     methods: {
         handleAddToCartClick(){
             console.log('add to cart click');
-            var newProduct= {
-                productId: this.productId,
-                productCode: this.productCode,
-                productName: this.productName,
-                mainPicUrl: this.mainPicUrl,
-                unitPrice: this.price,
-                quantity: this.quantity
-            };
+            var myShoppingCartJson = localStorage['myShoppingCartJson'];
+            this.myShoppingCart = myShoppingCartJson ? JSON.parse(myShoppingCartJson) : [];
 
-            this.myShoppingCart.push(newProduct);
-            localStorage['myShoppingCartJson']=JSON.stringify(this.myShoppingCart);
+            var cartProduct = this.myShoppingCart.find(p => p.productId === this.productId);
+            if (cartProduct) {
+                console.log('cart product exist');
+                var originQuantity = parseInt(cartProduct.quantity);
+                var addQuantity = parseInt(this.quantity);
+                cartProduct.quantity = originQuantity + addQuantity;
+            } else {
+                cartProduct = {
+                    productId: this.productId,
+                    productCode: this.productCode,
+                    productName: this.productName,
+                    mainPicUrl: this.mainPicUrl,
+                    unitPrice: this.price,
+                    quantity: this.quantity
+                };
+                this.myShoppingCart.push(cartProduct);
+            }
+
+            localStorage['myShoppingCartJson'] = JSON.stringify(this.myShoppingCart);
             this.$message.success('添加购物车成功');
         },
         getProductById() {
